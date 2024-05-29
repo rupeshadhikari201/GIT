3801172c329eedb8db999c832a91205dc75403bc
Rupesh <rupeshaadhikari1@gmail.com>
2024-05-29 19:57:57 +0530

    updated the email to hostinger

--- backend/settings.py
+++ backend/settings.py
@@ -119,11 +119,15 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.hostinger.com '
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = '21bcs11201@gmail.com'
-EMAIL_HOST_PASSWORD = 'uoba zdxf aucb uxih'
+# EMAIL_PORT = 587
+EMAIL_PORT = 465
+# EMAIL_HOST_USER = '21bcs11201@gmail.com'
+EMAIL_HOST_USER = 'gokap@gokapinnotech.com'
+# EMAIL_HOST_PASSWORD = 'uoba zdxf aucb uxih'
+EMAIL_HOST_PASSWORD = 'Gokap@2024'
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
