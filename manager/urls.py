06440e3cf34ae16a6b580bcc9e669446a935b283
YaredAlex <yareda25@gmail.com>
2024-08-28 15:53:06 +0530

    getting all kind of projects and assigned_projects to assigned/projects

--- manager/urls.py
+++ manager/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('assign_project/', views.ProjectAssignView.as_view(), name='assign_project'),
-    path('assigned_projects/<int:frelancer_id>/', views.GetAssignedProjectUsingFrelancerID.as_view()),
+    path('assigned/projects/<int:frelancer_id>/', views.GetAssignedProjectUsingFrelancerID.as_view()),
+    path('project/all',views.GetAllProject.as_view(),name='get_all_project'),
+    path('applied/freelancers/<int:project_id>/', views.AppliedFreelancersVeiw.as_view(), name='applied_freelancer'),
+    #search users 
 ]
