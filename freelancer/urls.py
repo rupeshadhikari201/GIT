203c7a63c708689331ce07a16bc227e237231c5f
YaredAlex <yareda25@gmail.com>
2024-08-29 21:30:02 +0530

    change details/ to detail/ and all_detail to all/

--- freelancer/urls.py
+++ freelancer/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('create/', views.FreelancerCreationView.as_view(), name='freelancer'),
-    path('details/', views.FreelancerDetails.as_view(), name='freelancer_details'),
+    path('detail/', views.FreelancerDetails.as_view(), name='freelancer_details'),
     path('update/', views.UpdateFreelancerView.as_view(), name="update_frelancer"),
     path('apply/', views.ApplyProjectView.as_view(), name='apply_project'),
-    path("all_details/", views.GetDetailsOfFrelancers.as_view()),
+    path("all/", views.GetDetailsOfFrelancers.as_view()),
     path('applied/projects/',views.GetAppliedProject.as_view(), name='get_applied_project'),
     path('search/', views.FreelancerSearchView.as_view(), name='freelancer-search'),
 ]
