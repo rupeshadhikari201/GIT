2399a2b2aa92ba8d96684c1ed440a2ac02a91244
YaredAlex <yareda25@gmail.com>
2024-08-29 21:52:19 +0530

    change api/register to api/user and api/freelancer

--- backend/urls.py
+++ backend/urls.py
@@ -26,9 +26,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('api/register/', include('register.urls')),
+   path('api/user/', include('register.urls')),
    path('api/client/', include('client.urls')),
-   path('api/freelacer/', include('freelancer.urls')),
+   path('api/freelancer/', include('freelancer.urls')),
    path('api/project/', include('project.urls')),
    path('api/payment/', include('payment.urls')),
    path('api/manager/', include('manager.urls')),
