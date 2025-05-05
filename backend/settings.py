c0d18c057aa320a6af03472b2b3dc7b6edec096b
YaredAlex <yareda25@gmail.com>
2025-05-05 17:47:00 +0530

    changed dburl to neon

--- backend/settings.py
+++ backend/settings.py
@@ -124,26 +124,26 @@ DATABASES = {
     #     "PORT": '5432',
     #     "PASSWORD": 'gokap123'
     # },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "gokapdb",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PORT": '5432',
-        "PASSWORD": 'root'
-    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "gokapdb",
+    #     "USER": "postgres",
+    #     "HOST": "localhost",
+    #     "PORT": '5432',
+    #     "PASSWORD": 'root'
+    # },
   
-    # 'default': {
-    # 'ENGINE': 'django.db.backends.postgresql',
-    # 'NAME': os.getenv('PGDATABASE'),
-    # 'USER': os.getenv('PGUSER'),
-    # 'PASSWORD': os.getenv('PGPASSWORD'),
-    # 'HOST': os.getenv('PGHOST'),
-    # 'PORT': os.getenv('PGPORT', 5432),
-    # 'OPTIONS': {
-    #   'sslmode': 'require',
-    # },
-    # },
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('PGDATABASE'),
+    'USER': os.getenv('PGUSER'),
+    'PASSWORD': os.getenv('PGPASSWORD'),
+    'HOST': os.getenv('PGHOST'),
+    'PORT': os.getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+    },
     # 'default': dj_database_url.config(
     #     default=os.environ.get('DATABASE_URL'),
     #     conn_max_age=600,
